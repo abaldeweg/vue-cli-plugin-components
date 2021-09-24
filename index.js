3b8ca3215bb92bb16575c21936d0fdef1013d37a
André Baldeweg <kontakt@andrebaldeweg.de>
2021-09-24 14:23:22 +0200

    fix path

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const cli = require('./cli')
 
 module.exports = (api, options) => {
   api.configureWebpack((config) => {
-    config.resolve.alias['~b'] = __dirname + '/.framework'
+    config.resolve.alias['~b'] = __dirname + '/../../../.framework'
     config.entry.app = ['./.framework/main.js']
   })
 
   api.chainWebpack((config) => {
     config.plugin('html').tap((args) => {
-      args[0].template = __dirname + '/.framework/public/index.html'
+      args[0].template = './.framework/public/index.html'
       return args
     })
   })
